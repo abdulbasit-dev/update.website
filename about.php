0ae6557c6e87043fc71b891ae0b9aacf7db03f58
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-01-09 12:44:16 +0300

    first commit

---
+++ about.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="./style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <title>968 hack</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<link rel="stylesheet" href="./about.css">
+<body>
+    <nav>
+        <div class="logo">968 HACK</div>
+        <input type="checkbox" id="click">
+        <label for="click" class="menu-btn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <ul>
+            <li><a class="active" href="./index.php">Home</a></li>
+            <li><a href="./hacking.php">Gallery</a></li>
+            <li><a href="./about.php">About</a></li>
+            <li><a href="./hacking.php">Hacking</a></li>
+        </ul>
+    </nav>
+        </div>
+        <h2>بەخیربێ من ئامانجم لەم سایتە ئەوەیە کە کورد بەرەو پیش بەرم لە تەکنەلۆجیادا و بتوان خۆیان بپاریزن و بۆ هەرکیشەیەکیش لە سۆشیال میدا توشت بوە ئەتوانی پەیوەندیمان پیوە کەی</h2>
+        <h1>DEVELOPER.WIBSITE---RoMaNy 721</h1>
+        <section class="footer">
+            <div class="social">
+               <a href="#"><i class="fab fa-facebook-f"></i></a>
+               <a href="#"><i class="fab fa-instagram"></i></a>
+               <a href="#"><i class="fab fa-snapchat"></i></a>
+               <a href="#"><i class="fab fa-telegram"></i></a>
+            </div>
+            <ul class="list">
+                <li>
+                    <a href="./index.php">Home</a>
+                </li>
+                <li>
+                    <a href="./about.php">About</a>
+                </li>
+                <li>
+                    <a href="./Hacking.php">Hacking</a>
+                </li>
+                <li>
+                    <a href="#">Gallery</a>
+                </li>
+            </ul>
+            <p class="copyright">
+                968 Hacker @ 2021
+            </p>
+        </section>
+</body>
+</html>
